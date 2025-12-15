d90585177154091a0a46a4865271d77068baf1e2
reet-571099 <khanchandani.reet@gmail.com>
2025-12-15 11:15:21 -0800

    cors issue

--- src/lib/portfolioData.ts
+++ src/lib/portfolioData.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://0.0.0.0:5001/";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001";
 
 interface StrategyStocks {
   [key: string]: {
